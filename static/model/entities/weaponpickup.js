089b94ed98c8804e070673501166d95be29a714a
terrygonguet <terry@gonguet.com>
2017-10-12 21:28:55 +0100

    Fixed WeaponPickup

--- static/model/entities/weaponpickup.js
+++ static/model/entities/weaponpickup.js
@@ -6,7 +6,7 @@ class WeaponPickup extends Pickup {
    * @param {String} weaponclass : The name of the weapon class
    */
   constructor(id, position=$V([0,0]), weaponclass) {
-    super(id, position, 6, "#EEE", e => e.weapon = new Hikari[weaponclass]());
+    super(id, position, 6, "#EEE", e => e.setWeapon(new Hikari[weaponclass]()));
     this.weaponclass = weaponclass;
     this.graphics.c().f(this.color)
                  .mt(0,-this.radius)
